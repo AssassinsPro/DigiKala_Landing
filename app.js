cb782a6cd6bfc2ccf2ebcbec4c0d4d68250a48cf
shayan <143071027+shayanUNK@users.noreply.github.com>
2025-05-31 23:39:56 +0330

    add( alpine.js )
    Download alpine.js to the project.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-    document.getElementById('search-container').addEventListener('click', function() {
-        document.getElementById('search-input').focus();
-    });
+// import alpine.js in js file
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+// So that when we click on the search box div, the input inside the box is also clicked.
+document
+  .getElementById("search-container")
+  .addEventListener("click", function () {
+    document.getElementById("search-input").focus();
+  });
