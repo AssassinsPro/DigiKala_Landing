4abaeee6d6c08acd3be563c3a7b516e040983cba
Hadixp <hadi.b.2002@gmail.com>
2025-06-01 04:01:48 +0430

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ window.Alpine = Alpine;
 Alpine.start();
 
 // So that when we click on the search box div, the input inside the box is also clicked.
-document
-  .getElementById("search-container")
-  .addEventListener("click", function () {
+document.getElementById("search-container").addEventListener("click", function () {
     document.getElementById("search-input").focus();
-  });
+});
+    
