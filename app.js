f62e6d9c50f64cd7353d4050a5707e51bc5e9a28
shayan <143071027+shayanUNK@users.noreply.github.com>
2025-06-05 05:26:58 +0330

    add( offer slider )
    complete offer slider section.

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-// import alpine.js in js file
-import Alpine from "alpinejs";
-
-window.Alpine = Alpine;
-
-Alpine.start();
-
 // So that when we click on the search box div, the input inside the box is also clicked.
-document.getElementById("search-container").addEventListener("click", function () {
+document
+  .getElementById("search-container")
+  .addEventListener("click", function () {
     document.getElementById("search-input").focus();
-});
+  });
 
 // offer slider swiper scripts
-    let swiper = new Swiper(".mySwiper", {
-      pagination: {
-        el: ".swiper-pagination",
-      },
-    });
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 2500,
+  },
+});
